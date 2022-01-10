08064e92f8c2d62a2cf01cc765e6c8f06aa022c5
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-09 18:20:33 -0600

    metodo post funcionando

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app:main");
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 require("dotenv").config();
 //GET
 app.get("/", (req, res) => {
@@ -15,5 +16,27 @@ app.get("/", (req, res) => {
   ];
   res.json(data);
 });
+
+app.get("/students", (req, res) => {
+  const students = [
+    {
+      name: "Ernesto Guevara Guzmán",
+      age: 16,
+    },
+    {
+      name: "Fernando Gachuz Guzmán",
+      age: 23,
+    },
+  ];
+
+  res.json(students);
+});
+
+app.post( "/students", (req, res) => {
+
+  res.json("datos recibidos");
+  debug(req.body);
+   
+})
 
 app.listen(PORT, () => debug(`servidor iniciado en http://localhost:${PORT}`));
