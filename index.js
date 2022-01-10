168693a9bdfe2666d97642846a2b0e03bf35556f
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-09 20:16:07 -0600

    creacion de crud basico

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const debug = require("debug")("app:main");
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
       name: "Ricardo",
       lastName: "Guevara",
       hoobie: "Chess",
-      age: 23,
+      age: 21,
     },
   ];
   res.json(data);
@@ -23,20 +24,27 @@ app.get("/students", (req, res) => {
       name: "Ernesto Guevara Guzmán",
       age: 16,
     },
-    {
-      name: "Fernando Gachuz Guzmán",
-      age: 23,
-    },
   ];
 
   res.json(students);
 });
 
-app.post( "/students", (req, res) => {
-
+app.post("/students", (req, res) => {
+  const { name, age } = req.body;
+  debug(name, age);
   res.json("datos recibidos");
-  debug(req.body);
-   
-})
+});
+
+app.put("/students/:id", (req, res) => {
+  const id = req.params.id;
+  debug(id);
+  res.json("datos recibidos para actualizar");
+});
+
+app.delete("/students/:id", (req, res) => {
+  const id = req.params.id;
+  debug(id);
+  res.json("datos recibidos para eliminar");
+});
 
 app.listen(PORT, () => debug(`servidor iniciado en http://localhost:${PORT}`));
