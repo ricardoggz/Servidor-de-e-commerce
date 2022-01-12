3c9ee84037043afa0e1a6b7f40b86acaed14d713
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-11 21:53:36 -0600

    Schema de usuarios creado, falta montarlo

--- src/database/index.js
+++ src/database/index.js
@@ -2,19 +2,12 @@ const mongoose = require("mongoose");
 const debug = require("debug")("app:database");
 require("dotenv").config();
 
-//conexión con mongo
-/*mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    debug(`Conexión con mongo atlas exitosa`);
-  })
-  .catch((error) => debug(error));*/
-
-const dbConnection = () =>
+const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => debug(`Conexion con mongo atlas exitosa`))
-    .catch((error) => debug(error));
+    .catch((error) => debug("Conexion fallida", error));
+};
 
 module.exports = {
   dbConnection,
--- src/models/users.js
+++ src/models/users.js
@@ -1,1 +1,14 @@
-//esquemas pendientes
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("User", userSchema);
