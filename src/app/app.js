9333f4bfb0b82918bd7e0fa47828563d293921d7
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-11 19:39:53 -0600

    refactor de app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const routesApp = require("../routes/routes");
 const { dbConnection } =require("../database/index");
-dbConnection();
 const app = express();
 app.use(express.json());
 app.use("/", routesApp);
+dbConnection();
 module.exports = app;
