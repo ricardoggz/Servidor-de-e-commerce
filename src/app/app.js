fa8d6d6a6266ea7596f6326e6a9b8599aa090688
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-14 12:07:52 -0600

    libreria cors instalada

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const routesApp = require("../routes/routes");
 const { dbConnection } =require("../database/index");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use("/", routesApp);
 dbConnection();
