8cb8e7dc0f9dc7b55a408369d40eb37e55338804
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-15 15:13:36 -0600

    comando de start creado en package json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("./src/app/app");
 const debug = require("debug")("app:main");
 const { Config } = require("./src/config/config");
 
-app.listen(Config.port, () =>
-  debug(`servidor iniciado en http://localhost:${Config.port}`)
-);
+app.listen(Config.port, () => {
+  debug(`servidor iniciado en http://localhost:${Config.port}`);
+  console.log(`servidor iniciado en ${Config.port}`);
+});
