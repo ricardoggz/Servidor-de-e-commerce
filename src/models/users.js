10f43a3af9846241a19f5e576392e34b9b7f7c9f
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-25 18:15:53 -0600

    validacion unica de usuarios creada

--- src/models/users.js
+++ src/models/users.js
@@ -1,22 +1,27 @@
 const { Schema, model } = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new Schema({
   name: {
     type: String,
     required: true,
   },
-  lastName: {
+  userName: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+userSchema.plugin(uniqueValidator, { message: "Nombre de usuario existente" });
 
 module.exports = model("User", userSchema);
